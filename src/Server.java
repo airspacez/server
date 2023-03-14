a0272d282cf8a85f6c98a4638749bc63f73fff77
Leonid <airspacece@mail.ru>
2023-03-14 10:41:53 +1000

    first commit

---
+++ src/Server.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8888)) {
+            System.out.println("Ожидание подключения клиента");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Клиент подключен: " + socket.getInetAddress().getHostAddress());
+
+                try (InputStream inputStream = socket.getInputStream();
+                     OutputStream outputStream = socket.getOutputStream()) {
+
+                    byte[] buffer = new byte[65536];
+                    int bytesRead;
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        String msg = new String(buffer, 0, bytesRead);
+                        if (msg.trim().equals("QUIT"))
+                            break;
+                        System.out.println("СИСТЕМНОЕ СООБЩЕНИЕ: " + msg);
+                        outputStream.write(msg.getBytes());
+                    }
+                }
+                System.out.println("Клиент отключен");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
